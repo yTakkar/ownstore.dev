5c312ed5e17528918507026195b8787cdc03cc98
Faiyaz Shaikh <www.shtakkar@gmail.com>
2021-11-30 12:56:53 +0530

    feat: update seo description

--- utils/seo/home.ts
+++ utils/seo/home.ts
@@ -6,9 +6,7 @@ import { getHomePageUrl } from '../home'
 export const prepareHomePageSeo = (): IAppSeoProps => {
   return {
     title: `Create your own online store with a suite of configurable projects | ${appConfig.global.app.name}`,
-    description: `OwnStore is an end-to-end e-commerce solution that can be configured according to your business needs. To simply put,
-    you will receive a completely configurable system (set of projects) to set up your own online store. And
-    yes, lifetime support too.`,
+    description: `OwnStore is an end-to-end e-commerce solution that can be configured according to your business needs.`,
     canonical: `${appConfig.global.baseUrl}${getHomePageUrl()}`,
     keywords: [
       'online store',
